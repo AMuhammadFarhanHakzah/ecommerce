d8baf354b7b032032d4131a28967676541e1960d
AMuhammadFarhanHakzah <amf21022002@gmail.com>
2024-04-07 15:31:08 +0800

    creating homepage

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -4,9 +4,14 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <title>{{ $title ?? 'Page Title' }}</title>
+        <title>{{ $title ?? 'M1l1Ze' }}</title>
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @livewireStyles
     </head>
-    <body>
-        {{ $slot }}
+    <body class="bg-slate-200 dark:bg-slate-700">
+        <main>
+            {{ $slot }}
+        </main>
+        @livewireScripts
     </body>
 </html>
