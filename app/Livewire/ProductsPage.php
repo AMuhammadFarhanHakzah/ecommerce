5fffabb4f77d9e6906d333880a3247ed5479ec7e
AMuhammadFarhanHakzah <amf21022002@gmail.com>
2024-04-17 08:20:46 +0800

    product page addToCart method

--- app/Livewire/ProductsPage.php
+++ app/Livewire/ProductsPage.php
@@ -2,6 +2,8 @@
 
 namespace App\Livewire;
 
+use App\Helpers\CartManagement;
+use App\Livewire\Partials\Navbar;
 use App\Models\Brand;
 use App\Models\Category;
 use App\Models\Product;
@@ -33,6 +35,13 @@ class ProductsPage extends Component
 
     #[Url]
     public $sort = 'latest';
+
+    //add to cart method
+    public function addToCart($product_id){
+        $total_count = CartManagement::addItemToCart($product_id);
+
+        $this->dispatch('update-cart-count', total_count: $total_count)->to(Navbar::class);
+    }
 
     public function render()
     {
